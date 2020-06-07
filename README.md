06f23e1341b4713c6baaab77f51e512950671caa
Marcel Thimm <marcel.thimm@rewe-digital.com>
2020-06-07 16:06:41 +0200

    implement -n param

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Command line tool to calculate murmur2 hash (as used by Kafka to determine parti
 Go programming language implementation of MurmurHash2, based on the works by [Austin Appleby](https://code.google.com/p/smhasher/) and [David Irvine](https://github.com/aviddiviner/go-murmur)
 
 ## Usage
-Uses 60 buckets by default
+Uses 60 buckets by default. To use customize the number of buckets, use ```-n <number>```
 
-    > murmur2 my-key
-    9
+    > murmur2 hash-me
+    46
+    > murmur2 -n 30 hash-me
+    16
 
 ## License
-MIT(LICENSE)
+[MIT](LICENSE)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
     "fmt"
     "os"
+    "strconv"
 )
 
 func toPositive(input uint32) (pos uint32) {
@@ -11,17 +12,34 @@ func toPositive(input uint32) (pos uint32) {
   return
 }
 
-func partition(input uint32) (part uint32) {
-  part = input % 60
+func partition(input uint32, noOfPartitions uint32) (part uint32) {
+  part = input % noOfPartitions
+  return
+}
+
+func keyAndPartitions(args []string) (key string, noOfPartitions uint32) {
+  noOfPartitions = 60
+  if (len(os.Args) == 4) {
+    if (os.Args[1] == "-n") {
+      noOfPartitions64, _ := strconv.ParseUint(os.Args[2], 0, 32)
+      noOfPartitions = uint32(noOfPartitions64)
+      key = os.Args[3]
+    } else {
+      fmt.Println("use -n to give number of partitions")
+    }
+  } else {
+    key = os.Args[1]
+  }
   return
 }
 
 func main() {
-    if (len(os.Args) == 1) {
-      fmt.Println("no args given")
-    } else {
-      arg := os.Args[1]
-      fmt.Println(partition(toPositive(MurmurHash2([]byte(arg)))))
-    }
+  if (len(os.Args) == 1) {
+    fmt.Println("no args given")
+    return
+  }
+
+  key, noOfPartitions := keyAndPartitions(os.Args)
+  fmt.Println(partition(toPositive(MurmurHash2([]byte(key))), noOfPartitions))
 }
 
